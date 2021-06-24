60bf36d82b1fb3b51e0f7dc7a4367e86a37674ac
Chris <chris@chris-agnew.com>
2021-06-24 19:17:55 -0400

    Clean up and ready to start project

---
+++ components/ContactBox.js
@@ -0,0 +1,9 @@
+const ContactBox = () => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default ContactBox;
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>contact details</h1>
+    </div>
+  );
+};
+
+export default Contact;
