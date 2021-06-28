5fe8c52d981c0c919b065515f9600d3f33150d8d
Chris <chris@chris-agnew.com>
2021-06-27 21:23:07 -0400

    Change Social Icon Colors and hover colors

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className="flex">
     
-          <div className="flex items-center justify-center w-1/2 bg-black md:pl-5 text-light-blue ">
-        <h3 className="mr-3 text-3xl md:text-5xl"><a href="https://www.facebook.com/vaporrevohio" target="_blank"><FaFacebookSquare/> </a></h3>
-        <h3 className="mr-3 text-3xl md:text-5xl"><a href="https://www.instagram.com/vapor_rev/" target="_blank"><FaInstagramSquare/> </a> </h3>
-        <h3 className="text-3xl md:text-5xl"><a href="https://open.spotify.com/playlist/1dyH8tgajIVj9KF2wv8U2l" target="_blank"><FaSpotify/> </a></h3>
+          <div className="flex items-center justify-center w-1/2 bg-black md:pl-5 text-dark-blue ">
+        <h3 className="mr-3 text-3xl md:text-5xl hover:text-light-blue"><a href="https://www.facebook.com/vaporrevohio" target="_blank"><FaFacebookSquare/> </a></h3>
+        <h3 className="mr-3 text-3xl md:text-5xl hover:text-light-blue"><a href="https://www.instagram.com/vapor_rev/" target="_blank"><FaInstagramSquare/> </a> </h3>
+        <h3 className="text-3xl md:text-5xl hover:text-light-blue"><a href="https://open.spotify.com/playlist/1dyH8tgajIVj9KF2wv8U2l" target="_blank"><FaSpotify/> </a></h3>
       </div>
       <div className="flex flex-col items-center justify-center w-1/2 p-5 text-white bg-black ">
         <h1 className="text-lg" >Vapor Rev</h1>
--- components/Navbar.js
+++ components/Navbar.js
@@ -19,10 +19,10 @@ const Navbar = () => {
     
       </div>
      
-      <div className="flex items-center justify-center w-full mt-5 mb-5 text-4xl text-center bg-black space-around text-light-blue md:w-1/3">
-        <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.facebook.com/vaporrevohio" target="_blank"><FaFacebookSquare /> </a></h3>
-        <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.instagram.com/vapor_rev/" target="_blank"><FaInstagramSquare/> </a> </h3>
-        <h3 className=" hover:text-dark-blue"><a href="https://open.spotify.com/playlist/1dyH8tgajIVj9KF2wv8U2l" target="_blank"><FaSpotify/> </a></h3>
+      <div className="flex items-center justify-center w-full mt-5 mb-5 text-4xl text-center bg-black space-around text-dark-blue md:w-1/3">
+        <h3 className="mr-3 hover:text-light-blue"><a href="https://www.facebook.com/vaporrevohio" target="_blank"><FaFacebookSquare /> </a></h3>
+        <h3 className="mr-3 hover:text-light-blue"><a href="https://www.instagram.com/vapor_rev/" target="_blank"><FaInstagramSquare/> </a> </h3>
+        <h3 className=" hover:text-light-blue"><a href="https://open.spotify.com/playlist/1dyH8tgajIVj9KF2wv8U2l" target="_blank"><FaSpotify/> </a></h3>
       </div>
     
       <div className="items-center hidden md:justify-center text-light-blue md:flex lg:justify-start md:w-1/3">
