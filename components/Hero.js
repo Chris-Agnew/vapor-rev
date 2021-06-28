f1b9c171d07ac650c4eadc5af3ffcdedc7fd4ced
Chris <chris@chris-agnew.com>
2021-06-27 23:36:39 -0400

    Fix responsive bug

--- components/Hero.js
+++ components/Hero.js
@@ -26,18 +26,18 @@ const Hero = () => {
     
         
         <div className="absolute top-0 z-50 flex flex-col justify-center w-3/4 h-full p-6 left-10 md:left-40 md:p-3 align-center">
-          <h1 className="z-10 mt-5 mb-4 text-lg text-center text-gray-100 md:text-3xl">
+          <h1 className="z-10 mt-5 mb-4 text-xl text-center text-gray-200 md:text-3xl">
             Vapor Rev is a vape shop in Willoughby.
           </h1>
-          <p className="z-10 text-xs text-center text-white ">
+          <p className="z-10 text-sm text-center text-gray-200 md:text-3xl">
             We are open and have returned to our normal scheduled store hours.
             If you would like to place a pickup order, please fill out the form
             below or click the button to call us.
           </p>
-          <div className="flex justify-center align-center">
+          <div className="flex justify-center mb-10 align-center">
             <button
               type="button"
-              className="p-2 mt-2 text-gray-200 rounded-lg bg-light-blue hover:bg-dark-blue"
+              className="p-2 mt-2 text-sm text-gray-200 rounded-lg bg-light-blue hover:bg-dark-blue sm:text-base"
             >
               <a href="tel:4405714341">Call us at: 440.571.4341</a>
             </button>
