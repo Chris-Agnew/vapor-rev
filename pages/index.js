7c99db2a4a5ad90feee605d8d91daa8fab80659f
Chris <chris@chris-agnew.com>
2021-06-27 20:29:18 -0400

    Layout components for homepage

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,28 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
+import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="font-nunito">
-      <Navbar />
-      <Hero />
-      <Services />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Vapor Rev | Vape shop in Willougby, Ohio</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          description="Vapor Rev is a vape shop in Willougby Ohio. Vapor Rev is a one stop shop for everything vape related. Stop in today!"
+          keywords="Vape,shop,willoughby, ohio,vapor,rev"
+        />
+      </Head>
+      <div className="w-full h-full font-nunito">
+        <Navbar />
+        <Hero />
+        <Services />
+        <ContactForm />
+        <Footer />
+      </div>
+    </>
   );
 }
