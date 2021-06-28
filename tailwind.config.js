6059bea885e197f2e6b24a5d233b8a1c09cc48a0
Chris <chris@chris-agnew.com>
2021-06-27 20:26:45 -0400

    Rebuild CSS File

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'nunito' :['Nunito']
+        nunito: ["Nunito"],
       },
-      textColor: {
-        'light-blue': '#0A9DFF',
-        'dark-blue': '#0A9DFF',
-        'coral-red': '#E63946',
-        'teal': '#0E7C7B',
-
-        
-      }
+      colors: {
+        "light-blue": "#0A9DFF",
+        "dark-blue": "#003459",
+        "rose-red": "#C2095A",
+        "mandarin": "EF8354",
+        "dark-red": "8D0801",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
