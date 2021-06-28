b559461c69a4dba55553ab5e4fdd91b1efed9982
Chris <chris@chris-agnew.com>
2021-06-27 20:36:00 -0400

    EDIT: some styling changes to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
     
       </div>
      
-      <div className="flex items-center justify-center w-full mt-5 mb-5 mr-2 text-4xl text-center bg-black space-around text-light-blue ">
+      <div className="flex items-center justify-center w-full mt-5 mb-5 text-4xl text-center bg-black space-around text-light-blue ">
         <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.facebook.com/vaporrevohio" target="_blank"><FaFacebookSquare /> </a></h3>
         <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.instagram.com/vapor_rev/" target="_blank"><FaInstagramSquare/> </a> </h3>
         <h3 className=" hover:text-dark-blue"><a href="https://open.spotify.com/playlist/1dyH8tgajIVj9KF2wv8U2l" target="_blank"><FaSpotify/> </a></h3>
