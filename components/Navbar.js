0f22431e63392778021a2eb780ee8905913f4e59
Chris <chris@chris-agnew.com>
2021-06-27 21:06:23 -0400

    Fix nav links width

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,8 @@ import {FaFacebookSquare, FaInstagramSquare, FaSpotify} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-full bg-black md:flex-row ">
-      <div className="flex items-center justify-center w-full h-full ">
+    <div className="flex flex-col items-center justify-center w-full h-full bg-black md:flex-row ">
+      <div className="flex items-center justify-center w-1/3 h-full ">
         <Link href="/">
           <a>
             <Image
@@ -19,13 +19,13 @@ const Navbar = () => {
     
       </div>
      
-      <div className="flex items-center justify-center w-full mt-5 mb-5 text-4xl text-center bg-black space-around text-light-blue ">
+      <div className="flex items-center justify-center w-full mt-5 mb-5 text-4xl text-center bg-black space-around text-light-blue md:w-1/3">
         <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.facebook.com/vaporrevohio" target="_blank"><FaFacebookSquare /> </a></h3>
         <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.instagram.com/vapor_rev/" target="_blank"><FaInstagramSquare/> </a> </h3>
         <h3 className=" hover:text-dark-blue"><a href="https://open.spotify.com/playlist/1dyH8tgajIVj9KF2wv8U2l" target="_blank"><FaSpotify/> </a></h3>
       </div>
     
-      <div className="items-center hidden md:justify-center text-light-blue md:flex ">
+      <div className="items-center hidden md:justify-center text-light-blue md:flex lg:justify-start md:w-1/3">
         <Link href="#services">
           <a className="m-3 border-dark-blue hover:border-dark-blue hover:border-b-2">
             Services
