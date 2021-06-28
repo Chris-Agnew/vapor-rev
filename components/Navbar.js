bf9f15379b874f3d8b0d11a6f71ce8a6898633d4
Chris <chris@chris-agnew.com>
2021-06-27 23:23:47 -0400

    Fix Navbar responsive bug

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import {FaFacebookSquare, FaInstagramSquare, FaSpotify} from 'react-icons/fa'
 const Navbar = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full bg-black md:flex-row ">
-      <div className="flex items-center justify-center w-full h-full md:w-1/3 ">
+      <div className="flex items-center justify-center h-full md:w-1/3 ">
         <Link href="/">
           <a>
             <Image
@@ -20,7 +20,7 @@ const Navbar = () => {
     
       </div>
      
-      <div className="flex items-center justify-center w-full mt-5 mb-5 text-4xl text-center bg-black space-around text-light-blue md:w-1/3">
+      <div className="flex items-center justify-center w-1/3 mt-5 mb-5 text-4xl text-center bg-black space-around text-light-blue">
         <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.facebook.com/vaporrevohio" target="_blank"><FaFacebookSquare /> </a></h3>
         <h3 className="mr-3 hover:text-dark-blue"><a href="https://www.instagram.com/vapor_rev/" target="_blank"><FaInstagramSquare/> </a> </h3>
         <h3 className=" hover:text-dark-blue"><a href="https://open.spotify.com/playlist/1dyH8tgajIVj9KF2wv8U2l" target="_blank"><FaSpotify/> </a></h3>
