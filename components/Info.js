e0618f841b10f34ffb7147b9b8bc11a604516c2f
Chris <chris@chris-agnew.com>
2021-06-29 16:28:01 -0400

    ADD: Info Component

---
+++ components/Info.js
@@ -0,0 +1,26 @@
+import Map from './Map'
+import ContactForm from './ContactForm'
+
+const Info = () => {
+	return (
+		<div className="flex flex-col bg-gray-900 lg:flex-row">
+			<div className="flex items-center justify-center h-1/3 lg:w-1/3 ">
+				<Map />
+			</div>
+			<div className="h-1/3 lg:w-1/3">
+				<ContactForm />
+			</div>
+			<div className="flex items-center justify-center h-full p-5 m-3 text-gray-200">
+				<h1 className="text-center">Hours of Operation:</h1>
+				<ul className="text-center">
+					<li>Monday - Friday: 11am - 8pm</li>
+					<li>Saturday: 11am - 9pm</li>
+					<li>Sunday: 12pm - 4pm</li>
+					
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+export default Info
