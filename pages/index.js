e3e0a58303b3b3b3e1062dcd5fa8c8a10fc724cc
Chris <chris@chris-agnew.com>
2021-06-29 18:53:21 -0400

    ADD: Image to meta data for sharing links

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,16 @@ export default function Home() {
   return (
     <>
       <Head>
-       
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"/>
-        <meta name="description" content="Vapor Rev is a vape shop in Willougby Ohio. Vapor Rev is a one stop shop for everything vape related. Stop in today!" keywords="Vape,shop,willoughby, ohio,vapor,rev"/>
+          name="description"
+          content="Vapor Rev is a vape shop in Willougby Ohio. Vapor Rev is a one stop shop for everything vape related. Stop in today!"
+          keywords="Vape,shop,willoughby, ohio,vapor,rev"
+        />
+        <meta
+          property="og:image"
+          content="https://www.chris-agnew.com/logo.webp"
+        />
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <title>Vapor Rev | Vape shop in Willoughby, Ohio</title>
       </Head>
