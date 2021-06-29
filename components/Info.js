67d22fb9587ebe02b9a7fafedbff67f61cbece46
Chris <chris@chris-agnew.com>
2021-06-29 19:06:06 -0400

    Fix box alignment to fit with top row

--- components/Info.js
+++ components/Info.js
@@ -3,14 +3,14 @@ import ContactForm from "./ContactForm";
 
 const Info = () => {
   return (
-    <div className="flex flex-col bg-gray-900 lg:flex-row ">
-      <div className="flex items-center justify-center h-1/3 lg:w-1/3 ">
+    <div className="flex flex-col bg-gray-900 justify-evenly lg:flex-row ">
+      <div className="flex items-center justify-center lg:w-1/3">
         <Map />
       </div>
-      <div className=" lg:w-1/3">
+      <div className="lg:w-1/3">
         <ContactForm />
       </div>
-      <div className="flex items-center justify-center p-5 m-3 text-gray-200 lg:w-1/3">
+      <div className="flex items-center justify-center text-gray-200 lg:w-1/3">
         <div className="flex flex-col items-center justify-center ">
           <h1 className="text-center">Hours of Operation:</h1>
           <ul className="text-center">
