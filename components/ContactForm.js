f31956d65b16b9497525870b3453dd7b49dadf02
Chris <chris@chris-agnew.com>
2021-06-29 16:27:44 -0400

    Minor Styling changes

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -24,21 +24,21 @@ const ContactForm = () => {
   };
   return (
    <div id="contact" className="flex items-center justify-center bg-dark-blue">
-   <div className="w-full mt-1 rounded md:mt-0 md:w-1/4 bg-dark-blue">
-   <form onSubmit={handleSubmit}>
+   <div className="mt-1 rounded md:mt-0 bg-dark-blue">
+   <form  onSubmit={handleSubmit}>
       <div className="flex items-center justify-center m-5">
         <label htmlFor="name" className="text-xl text-gray-200">Name:</label>
-        <input type="text" id="name" placeholder="John Smith" className="p-2 ml-5 bg-gray-300 rounded-full outline-none" required />
+        <input type="text" id="name" placeholder=" John Smith" className="p-2 ml-5 bg-gray-300 rounded-full outline-none" required />
       </div>
       <div className="flex items-center justify-center m-5">
         <label htmlFor="email" className="text-xl text-gray-200">Email:</label>
-        <input type="email" id="email" placeholder="john@smith.com" className="p-2 ml-5 text-left bg-gray-300 rounded-full outline-none radius" required />
+        <input type="email" id="email" placeholder=" john@smith.com" className="p-2 ml-5 text-left bg-gray-300 rounded-full outline-none radius" required />
       </div>
-      <div className="flex items-center justify-center p-5">
+      <div className="flex items-center justify-center w-full h-full p-0">
         <div><label htmlFor="message" className="text-xl text-gray-200">Message:</label></div>
-        <textarea placeholder="Type your message here" id="message" required className="p-2 mt-5 ml-5 bg-gray-300 border-2 rounded outline-none h-28"/>
+        <textarea placeholder="Type your message here" id="message" required className="p-2 mt-5 ml-5 bg-gray-300 border-2 rounded outline-none "/>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center ">
       <button className="w-40 p-1 m-5 text-gray-200 border-2 border-black rounded-full outline-none hover:bg-light-blue hover:text-white " type="submit">{status}</button>
       </div>
       
