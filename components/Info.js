51f35da9f04cdfdeb3a7ca9dfeb54695da655fa7
Chris <chris@chris-agnew.com>
2021-06-30 17:47:13 -0400

    Add Address and phone content

--- components/Info.js
+++ components/Info.js
@@ -4,20 +4,25 @@ import ContactForm from "./ContactForm";
 const Info = () => {
   return (
     <div className="flex flex-col bg-gray-900 justify-evenly lg:flex-row ">
-      <div className="flex items-center justify-center lg:w-1/3">
+      <div className="flex items-center justify-center lg:w-1/2">
         <Map />
       </div>
-      <div className="lg:w-1/3">
+      {/* <div className="lg:w-1/3">
         <ContactForm />
-      </div>
-      <div className="flex items-center justify-center p-10 text-gray-200 lg:w-1/3">
+      </div> */}
+      <div className="flex flex-col items-center justify-center p-10 text-gray-200 lg:w-1/2">
         <div className="flex flex-col items-center justify-center ">
-          <h1 className="text-center">Hours of Operation:</h1>
-          <ul className="text-center">
-            <li>Monday - Friday: 11am - 8pm</li>
-            <li>Saturday: 11am - 9pm</li>
-            <li>Sunday: 12pm - 4pm</li>
+          <h1 className="text-center lg:text-3xl">Hours of Operation:</h1>
+          <ul className="text-center lg:text-2xl">
+            <li>Monday - Saturday: 11am - 8pm</li>
+            <li>Sunday: 12pm - 6pm</li>
           </ul>
+        </div>
+        <div className="flex flex-col items-center justify-center mt-10">
+          <h2 className="text-center md:text-2xl">440-571.4341</h2>
+          <h3 className="pt-2 text-center lg:text-2xl">
+            38669 MENTOR AVE UNIT B. WILLOUGHBY, OH, 44094
+          </h3>
         </div>
       </div>
     </div>
