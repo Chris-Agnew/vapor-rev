225fc2b627b66a05ad4a2d03fc282361c9e4cf04
Chris <chris@chris-agnew.com>
2021-06-30 17:45:55 -0400

    Change hero image

--- components/Hero.js
+++ components/Hero.js
@@ -2,28 +2,28 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <>
-      <div className="relative w-screen h-full bg-black md:w-full md:h-1/2">
+      <div className="relative items-center justify-center w-screen h-full text-center md:w-full md:h-1/2 ">
+        <div className="flex items-center justify-center w-full font-bold ">
+          <div className="flex items-center justify-center w-full bg-black">
+            <p className="p-2 text-sm bg-gray-200 border-8 border-black">
+              WARNING: This product contains nicotine. Nicotine is an addictive
+              chemical.
+            </p>
+          </div>
+        </div>
 
-        {/* <div className="absolute top-0 z-10 w-screen h-full md:h-full md:w-full md:flex md:flex-col md:justify-center align-center md:mx-auto">
+        <div className="absolute top-0 z-10 items-center w-screen h-full text-white md:h-full md:w-full md:flex md:flex-col md:justify-center md:mx-auto "></div>
+        <div>
           <Image
-            className="z-10"
-            src="https://www.chris-agnew.com/nicotine.jpeg"
+            src="/images/dark-final.jpg"
             layout="responsive"
-            height={150}
-            width={300}
+            alt="Vapor Rev Willoughby Ohio Shop"
+            height={400}
+            width={500}
           />
-        </div> */}
-     <div>
-     <Image 
-          src="https://www.chris-agnew.com/vape-shop-dark.jpg"
-          layout="responsive"
-          alt="Vapor Rev Willoughby Ohio Shop"
-          height={400}
-          width={500}
-        />
-     </div>
+        </div>
         <div className="absolute top-0 z-50 flex flex-col items-center justify-center w-3/4 h-full p-6 left-10 md:left-32 md:p-3">
-          <h1 className="z-10 mt-5 mb-4 text-xl text-center text-gray-200 md:text-3xl">
+          <h1 className="z-10 mt-5 mb-4 text-xl text-center text-gray-300 md:text-3xl">
             Vapor Rev is a vape shop in Willoughby.
           </h1>
           <p className="z-10 text-sm text-center text-gray-200 md:text-2xl md:m-5">
