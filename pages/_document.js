9ba5f850a5bcdf4b9813c1e053cac8622e02b935
Chris <chris@chris-agnew.com>
2021-07-10 17:53:49 -0400

    ADD: meta language data

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,7 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <meta http-equiv="content-language" content="en-us"></meta>
         </Head>
         <body>
           <Main />
