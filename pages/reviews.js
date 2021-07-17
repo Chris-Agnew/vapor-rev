9466d7d537659ad9aab1c3142b6392b32809a0e3
Chris <chris@chris-agnew.com>
2021-07-17 16:23:44 -0400

    Tidy up styles for review page

--- pages/reviews.js
+++ pages/reviews.js
@@ -3,11 +3,12 @@ import Review from "../components/Review";
 const reviews = () => {
   //   console.log(data);
   return (
-    <div className="flex flex-col items-center justify-center text-center bg-gray-800 md:flex-row">
-		<h2 className="text-center text-gray-200">See Vapor Rev Reviews from Google Reviews below</h2>    
-      <Review />
-    </div>
-	
+    <div className="flex flex-col items-center justify-center text-center bg-gray-800 ">
+		<h2 className="p-3 text-center text-gray-200">See Vapor Rev Google Reviews below </h2>      
+    <div className="flex flex-col flex-wrap items-center justify-center md:flex-row">
+	<Review />
+		</div> 
+	</div>
   );
 };
 
