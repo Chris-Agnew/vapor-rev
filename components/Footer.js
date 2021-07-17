9f9ad45e4a54a992d714bda913e542a7125fbdca
Chris <chris@chris-agnew.com>
2021-07-17 16:22:34 -0400

    Fix styling issues

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { FaFacebookSquare, FaInstagramSquare, FaSpotify } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="flex ">
-      <div className="flex items-center justify-center w-1/2 bg-black md:pl-5 text-light-blue ">
+      <div className="flex items-center justify-center w-2/5 pl-5 bg-black md:w-1/2 text-light-blue ">
         <h3 className="mr-3 text-3xl md:text-5xl hover:text-dark-blue">
           <a href="https://www.facebook.com/vaporrevohio" target="_blank">
             <FaFacebookSquare />{" "}
@@ -23,8 +23,8 @@ const Footer = () => {
           </a>
         </h3>
       </div>
-      <div className="flex flex-col items-center justify-center w-1/2 p-10 text-gray-200 bg-black ">
-        <h2 className="text-lg text-center">Vapor Rev Inc &copy; 2021</h2>
+      <div className="flex flex-col items-center justify-center w-3/5 p-5 text-gray-200 bg-black md:w-1/2 ">
+        <h2 className="text-center text-md">Vapor Rev Inc &copy; 2021</h2>
         <h2 className="text-xs text-center">Vape shop in Willoughby, Ohio</h2>
       </div>
     </div>
