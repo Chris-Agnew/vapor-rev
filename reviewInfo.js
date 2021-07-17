169f062b1d6e9a7fb46c756c2789d821cce41267
Chris <chris@chris-agnew.com>
2021-07-16 23:20:06 -0400

    dummy data while I wait for GMB Api to be approved

---
+++ reviewInfo.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    name: "Melisa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bre",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+];
+
+export default data;
