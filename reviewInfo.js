164d769b5e66c542f040785a0eccfa258d708fae
Chris <chris@chris-agnew.com>
2021-07-17 16:46:02 -0400

    ADD: people

--- reviewInfo.js
+++ reviewInfo.js
@@ -69,6 +69,636 @@ const data = [
     starRating: 4,
     id: "5",
   },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Melissa",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "1",
+  },
+  {
+    name: "Bree",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "2",
+  },
+  {
+    name: "Matt",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 5,
+    id: "3",
+  },
+  {
+    name: "Johnny",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "4",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
+  {
+    name: "Chris",
+    message:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a nihil praesentium labore odit excepturi.",
+    starRating: 4,
+    id: "5",
+  },
 ];
 
 export default data;
