7ece6082927934b4b337beccc81f871eebb4b6fb
Chris <chris@chris-agnew.com>
2021-07-17 16:24:38 -0400

    Change to have 2 columns of reviews

--- components/Review.js
+++ components/Review.js
@@ -3,7 +3,7 @@ import reviews from '../reviewInfo'
 const Review = () => {
 	return (
 		reviews.map((review) => 
-	<div key={review.id}className="flex flex-col flex-wrap items-center justify-center w-1/3 p-2 m-2 bg-gray-200 border-gray-300 rounded-md md:flex-row">
+	<div key={review.id} className="flex flex-col items-center justify-center w-3/4 p-2 m-2 bg-gray-200 border-gray-300 rounded-md md:w-1/3 ">
 		<h3 className="p-2 text-sm">{review.starRating} Stars</h3>
 		<h1 className="p-2 text-lg">{review.name}</h1>
 		<p className="p-2 text-xs">{review.message}</p>
