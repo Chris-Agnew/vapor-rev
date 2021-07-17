dbd55c0fe574627a1508641ae929799bc0daad9a
Chris <chris@chris-agnew.com>
2021-07-17 16:45:50 -0400

    ADD: People to dummy data

--- pages/reviews.js
+++ pages/reviews.js
@@ -1,4 +1,7 @@
-import Review from "../components/Review";
+// import Review from "../components/Review";
+import dynamic from 'next/dynamic'
+
+const Review = dynamic(() => import('../components/Review'))
 
 const reviews = () => {
   //   console.log(data);
