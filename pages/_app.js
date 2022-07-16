f0f69c593fdc91ad0aa53ff16f4be7d390bf29ab
Chris-Agnew <chris-agnew@pm.me>
2022-07-15 21:55:29 -0400

    add overflow hidden

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
-import 'tailwindcss/tailwind.css'
-import '../public/styles/globals.css'
+import "tailwindcss/tailwind.css";
+import "../public/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="overflow-hidden">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
