fc907f670db2dac09d43078686600f1b42202075
Chris-Agnew <chris-agnew@pm.me>
2022-07-15 21:54:19 -0400

    make map larger

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,14 @@
 const Map = () => {
-	return (
-		<div className="flex items-center justify-center p-2 m-3">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11918.912950411834!2d-81.3509408!3d41.6832123!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8831a974e085d375%3A0x6184cf3797fabdc1!2sVAPOR%20REV!5e0!3m2!1sen!2sus!4v1624919761234!5m2!1sen!2sus" width="300" height="300" loading="lazy"></iframe>
-		</div>
-	)
-}
+  return (
+    <div className="flex items-center justify-center p-2 m-3">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11918.912950411834!2d-81.3509408!3d41.6832123!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8831a974e085d375%3A0x6184cf3797fabdc1!2sVAPOR%20REV!5e0!3m2!1sen!2sus!4v1624919761234!5m2!1sen!2sus"
+        width="500"
+        height="500"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
