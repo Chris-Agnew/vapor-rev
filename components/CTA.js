4153fe114f74bce134b2e579964d402a5c33366e
Chris-Agnew <chris-agnew@pm.me>
2022-07-15 22:29:39 -0400

    fix positioning

--- components/CTA.js
+++ components/CTA.js
@@ -5,7 +5,7 @@ const Cta = ({ text, icon, image, textHover, href }) => {
   return (
     <a className="relative   group  flex justify-center" href={href}>
       <Image
-        className="absolute transition-opacity opacity-100 group-hover:opacity-50 object-center object-cover duration-500 "
+        className="absolute transition-opacity opacity-80 group-hover:opacity-50 object-center object-cover duration-500 "
         src={image}
         alt="CTA Image"
         layout="intrinsic"
@@ -13,13 +13,13 @@ const Cta = ({ text, icon, image, textHover, href }) => {
         height={700}
       />
 
-      <div className=" absolute top-[60%] my-3">
-        <p className="text-2xl font-bold text-gray-200 flex text-center justify-center items-center uppercase font-poppins">
+      <div className=" absolute  xl:top-[60%] top-[45%] my-3 ">
+        <p className="text-2xl lg:text-4xl font-bold text-gray-200 flex text-center justify-center items-center uppercase font-poppins ">
           {text}
           {icon}
         </p>
         <div className="transition-all transform translate-y-8 opacity-0  group-hover:opacity-100 group-hover:translate-y-0 duration-500 flex justify-center">
-          <p className="text-md w-4/5 text-center font-raleway text-gray-200 m-3">
+          <p className="text-sm w-4/5 md:text-xl text-center font-raleway text-gray-200 m-3 ">
             {textHover}
           </p>
         </div>
