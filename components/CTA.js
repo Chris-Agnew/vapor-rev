3d3a9f663aac71a2fd3110085e21212e65475986
Chris-Agnew <chris-agnew@pm.me>
2022-07-17 14:25:44 -0400

    add padding to paragraphs

--- components/CTA.js
+++ components/CTA.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Cta = ({ text, icon, image, textHover, href }) => {
   return (
-    <a className="relative   group  flex justify-center" href={href}>
+    <a className="relative group flex justify-center" href={href}>
       <Image
         className="absolute transition-opacity opacity-80 group-hover:opacity-50 object-center object-cover duration-500 "
         src={image}
@@ -19,7 +19,7 @@ const Cta = ({ text, icon, image, textHover, href }) => {
           {icon}
         </p>
         <div className="transition-all transform translate-y-8 opacity-0  group-hover:opacity-100 group-hover:translate-y-0 duration-500 flex justify-center">
-          <p className="text-sm w-4/5 md:text-md xl:text-lg text-center font-raleway text-gray-200 m-3 ">
+          <p className="text-sm w-4/5 md:text-md md:p-2 lxl:text-lg text-center font-raleway text-gray-200 m-3 ">
             {textHover}
           </p>
         </div>
